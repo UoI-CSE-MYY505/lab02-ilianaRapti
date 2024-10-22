89d12ff9435e7bbd67f56bd849e6812c13aab29c
Iliana <cs05128@uoi.gr>
2024-10-22 17:21:30 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -11,6 +11,19 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+li t0, 0 #immediate index = 0
+li t2, 0 #immediate result = 0
+loop:
+    beq t0, a1, done #if t0 equal a1(size of array), go to done
+    lw t1, 0(a0) #load word of t1 -> save it @a0 
+    beq t1, a2, res #if value = res, jump to res
+    addi t0, t0, 1 #index += 1
+    addi a0, a0, 4 #address += 4 (word = 4bytes)
+    j loop #jump to loop
+
+
+res:
+    mv a0, t2 #move t2(result) -> a0  
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
